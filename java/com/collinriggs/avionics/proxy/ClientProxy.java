c18ceb01fda3a02751af0cad4f8177d7a69fa864
ADeathlyTouch <ADeathyTouch@users.noreply.github.com>
2016-11-20 03:39:37 -0600

    Add files via upload

---
+++ java/com/collinriggs/avionics/proxy/ClientProxy.java
@@ -0,0 +1,31 @@
+package com.collinriggs.avionics.proxy;
+
+import com.collinriggs.avionics.Items.ModItems;
+import com.collinriggs.avionics.blocks.ModBlocks;
+
+import com.collinriggs.avionics.recipe.ModRecipes;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit(FMLPreInitializationEvent e) {
+        super.preInit(e);
+        ModBlocks.initModels();
+    }
+
+    @Override
+    public void init(FMLInitializationEvent e) {
+        super.init(e);
+        ModItems.registerRenders();
+        ModRecipes.init();
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent e) {
+        super.postInit(e);
+    }
+
+}
